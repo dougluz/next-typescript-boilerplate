1e7be2ff042a3368147c23c2ac08f4641ab20b7e
dougluz <dougs.2210@gmail.com>
2021-05-29 23:33:35 -0300

    feat: added const

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ type Props = {
   title: string
 }
 
+const test = 'nothing'
+
 export default function Home({ title = 'React Avançado' }: Props) {
   return (
     <div className={styles.container}>
